06c7a7f04009781b93f8603c64c8d8993b32a40a
Martin Olsson <martin@minimum.se>
2016-03-10 00:38:15 +0100

    Add rails config token

---
+++ src/rails_config.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e336769aa121b61447b6a51050ccd6e60a58980967c209e167fd7f8e6ba46e59eabf68871bf91d1c34b0eddb9ab9cccb0cbfe7eac792e7c427dbd1fa99b55065'
